9f8e8390d71e64bea69c567fdc5d9c94ec2c7e4e
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-05-17 14:36:52 +0200

    adding more precision

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ using Microsoft.Extensions.Configuration;
 
 
 
-var configuration =  new ConfigurationBuilder()
+var configuration = new ConfigurationBuilder()
      .AddJsonFile($"local.settings.json");
-            
+
 var config = configuration.Build();
 
 var numOfEvents = config.GetValue<int>("numOfEvents");
@@ -25,41 +25,41 @@ EventHubProducerClient producerClient = new EventHubProducerClient(
     eventHubName,
     new DefaultAzureCredential());
 
-// create a list of event batches which we will send to the event hub
-List<EventDataBatch> eventBatches = new List<EventDataBatch>();
+Stopwatch stopwatch = Stopwatch.StartNew();
 
-// create 20 batches of 2000 events each
-for (int i = 0; i < numOfBatches; i++)
-{
-    // Create a batch of events 
-    EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
-
-    for (int j = 0; j < numOfEvents; j++)
-    {
-        if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Batch {i} --- Event {j}"))))
-        {
-            // if it is too large for the batch
-            throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
-        }
-    }
-    eventBatches.Add(eventBatch);
-}
-
+// create i batches of j events each
 try
 {
-    // measure the time it takes to send the events to the event hub
-    Stopwatch stopwatch = Stopwatch.StartNew();
-    // iterate over the list of batches and send the events to the event hub
-    foreach (EventDataBatch eventBatch in eventBatches)
+    for (int i = 0; i < numOfBatches; i++)
     {
-        // Use the producer client to send the batch of events to the event hub
-        await producerClient.SendAsync(eventBatch);
-        Console.WriteLine($"A batch number {eventBatches.IndexOf(eventBatch)} with {numOfEvents} events has been published.");
+        // Create a batch of events 
+        EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+        for (int j = 0; j < numOfEvents; j++)
+        {
+            if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Batch {i} --- Event {j}"))))
+            {
+                // if it is too large for the batch
+                throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+            }
+        }
+        Task sendTask = producerClient.SendAsync(eventBatch);
+        sendTask.Wait();
+        if (sendTask.Exception != null)
+            Console.WriteLine(sendTask.Exception.Message);
+
+        Console.WriteLine($"A batch number {i} with {numOfEvents} events has been published.");
+        eventBatch.Dispose();
     }
-    stopwatch.Stop();
-    Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to send {numOfEvents * numOfBatches} events.");
 }
 finally
 {
     await producerClient.DisposeAsync();
 }
+stopwatch.Stop();
+Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} milliseconds to send {numOfEvents * numOfBatches} events.");
+// calculate the numOfEvents * numOfBatches / stopwatch.ElapsedMilliseconds as double with two decimal places
+
+Console.WriteLine($"The throughput per millisecond is {Math.Round((double)numOfEvents * numOfBatches / stopwatch.ElapsedMilliseconds, 2)} events per millisecond.");
+// calculate the numOfEvents * numOfBatches / stopwatch.ElapsedMilliseconds * 1000 as double with two decimal places
+Console.WriteLine($"The throughput per second is {Math.Round((double)numOfEvents * numOfBatches / stopwatch.ElapsedMilliseconds * 1000, 2)} events per second.");
